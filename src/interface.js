c9b385f67dce9a51e846d3c020867a4a89af269a
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-06 17:20:54 +0000

    updated interface.js with jQuery functions enabling the clicking of buttons to incite javascript outputs. Updated index.html to have button ids and to output jQuery text. User can see temp increasing/decreasing and psm on/off

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,37 @@
 $(document).ready(function() {
+  var thermostat = new Thermostat();
+  
+  $('#temperature').text(thermostat.temperature);
 
-    $("#tempUp").click(function() {
-        alert("you turned the temp up");
-    });
+  $("#tempUp").click(function() {
+      thermostat.up();
+      updateTemperature();
+  });
+
+  $("#tempDown").click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $("#psmOn").click(function() {
+    thermostat.powerSavingModeOn();
+    $('#psmon').text('PSM turned on');
+    updateTemperature();
+  });
+
+  $("#psmOff").click(function() {
+    thermostat.powerSavingModeOff();
+    $('#psmoff').text('PSM turned off');
+    updateTemperature();
+  });
+
+  $("#reset").click(function() {
+    alert("Temp reset to 20 degrees");
+    thermostat.reset();
+    updateTemperature();
+  });
+
+  function updateTemperature(){
+    $('#temperature').text(thermostat.temperature);
+  };
 });
