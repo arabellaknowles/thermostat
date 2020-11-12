9cbfe2c9aff80f45f41844887de610842a7b3534
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-12 11:47:42 +0000

    passed feature test testing that Down button decreases temp by 1 degree

--- spec/features/temperature_spec.rb
+++ spec/features/temperature_spec.rb
@@ -7,8 +7,13 @@ feature 'viewing the temperature' do
     expect(page).to have_content(20)
   end
 
-  it 'increased temp by 1 degree when clicking up' do
+  it 'increases temp by 1 degree when clicking Up' do
     click_button('Up')
     expect(page.find("#temperature")).to have_content(21)
+  end
+  
+  it 'decreases temp by 1 degree when clicking Down' do
+    click_button('Down')
+    expect(page.find("#temperature")).to have_content(19)
   end
 end
